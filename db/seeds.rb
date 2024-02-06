f45d0ca017de2fd4749d6af53ba9fece91748713
qyao <qyao4@academic.rrc.ca>
2024-02-06 14:46:27 -0600

    populate products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-product = Product.create(title: nil, price: -10, stock_quantity: -1)
 
-if product.persisted?
-  puts "Product was saved successfully, which should not happen."
-else
-  puts "Failed to save product, as expected:"
-  puts product.errors.full_messages
+# Test create an invalid product
+# product = Product.create(title: nil, price: -10, stock_quantity: -1)
+
+# if product.persisted?
+#   puts "Product was saved successfully, which should not happen."
+# else
+#   puts "Failed to save product, as expected:"
+#   puts product.errors.full_messages
+# end
+
+# Populate the products table with 676 products
+676.times do
+  Product.create!(
+    title: Faker::Commerce.product_name,
+    price: Faker::Commerce.price(range: 0.01..100.0),
+    stock_quantity: Faker::Number.between(from: 1, to: 100)
+  )
 end
+
+puts "Created 676 products."
