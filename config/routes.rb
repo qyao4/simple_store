cded95277b2df6b3d7200dee87356a2f13ad3712
qyao <qyao4@academic.rrc.ca>
2024-02-06 15:21:36 -0600

    show products

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'products', to: "products#index"
-
+  resources :products, only: [:index]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
