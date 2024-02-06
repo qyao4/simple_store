8d829a4347d494bc097725b616a68da3e7f4388a
qyao <qyao4@academic.rrc.ca>
2024-02-06 16:14:32 -0600

    add link to show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,12 @@
 class ProductsController < ApplicationController
+
+  # Index
   def index
     @products = Product.all
+  end
+
+  # Show action
+  def show
+    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :products, only: [:index]
+  resources :products, only: [:index, :show]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
